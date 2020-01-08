3a4af8e210dc76fadc0781a176c6463f99aa6373
Maynard Hartman <mjhartman@brighthouse.com>
2020-01-08 03:16:29 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,19 @@
 require_relative "./vehicle.rb"
+class vehicle 
+  attr_accessor :wheel_siZe, :wheel_number
+  
+  @@all = []
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  def go 
+    "Vroooooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+end
